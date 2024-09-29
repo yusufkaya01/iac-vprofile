5112586196f5b701906a24461857c350fa3c045d
yusuf_kaya <160890360+yusufkaya01@users.noreply.github.com>
2024-09-29 17:45:51 +0300

    Update variables.tf
    cluster name changed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "vprofile-eks"
+  default     = "vprofile-eks-ysf"
 }
 
 ##
